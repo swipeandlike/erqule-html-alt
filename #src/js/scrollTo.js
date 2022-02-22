26fff7a21f2da4b7d2afd563c7d0bff1c00cf899
Slava79198015981 <slava79198015981@gmail.com>
2022-02-22 13:57:35 +0400

    Other scripts

--- #src/js/scrollTo.js
+++ #src/js/scrollTo.js
@@ -1,5 +1,4 @@
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
-console.log(menuLinks)
 
 if (menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
@@ -17,11 +16,11 @@ if (menuLinks.length > 0) {
 
         menuLink.classList.add('menu__link--active');
 
-        // if (document.querySelector('.menu__icon').classList.contains('menu__icon--active')) {
-        //     document.querySelector('.menu__body').classList.remove('menu__body--active')
-        //     document.querySelector('.menu__icon').classList.remove('menu__icon--active')
-        //     document.body.classList.remove('_lock')
-        // }
+        if (document.querySelector('.menu__icon').classList.contains('menu__icon--active')) {
+            document.querySelector('.menu__body').classList.remove('menu__body--active')
+            document.querySelector('.menu__icon').classList.remove('menu__icon--active')
+            document.body.classList.remove('_lock')
+        }
 
         window.scrollTo({
             top:gotoBlockValue,
