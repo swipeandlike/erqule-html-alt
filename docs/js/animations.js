c74860a9acfa4e4cd276c58c561205d4d5760067
Slava79198015981 <slava79198015981@gmail.com>
2022-02-22 13:55:03 +0400

    Other (docs files)

--- docs/js/animations.js
+++ docs/js/animations.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const scrollItems = document.querySelectorAll('.scroll-item');
 
     const scrollAnimations = () => {
-        let windowCenter = window.innerHeight + window.scrollY;
+        let windowCenter = (window.innerHeight / 2) + window.scrollY;
         scrollItems.forEach(el => {
             let scrollOffset = el.offsetTop + (el.offsetHeight / 2);
             if (windowCenter >= scrollOffset) {
@@ -15,7 +15,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     scrollAnimations();
 
+    //low
+
+    const scrollItemsLow = document.querySelectorAll('.scroll-item--low');
+
+    const scrollAnimationsLow = () => {
+        let windowCenter = window.innerHeight + window.scrollY;
+        scrollItemsLow.forEach(el => {
+            let scrollOffset = el.offsetTop + el.offsetHeight;
+            if (windowCenter >= scrollOffset) {
+                el.classList.add('animation-class');
+            } else {
+                el.classList.remove('animation-class');
+            }
+        })
+    }
+
+    scrollAnimationsLow();
+
     window.addEventListener('scroll', () => {
         scrollAnimations();
+        scrollAnimationsLow();
     })
 });
